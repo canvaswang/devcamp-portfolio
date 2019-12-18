884f06fa03cafc92380a02028f826083f9f4c78e
cwang <cwang@hotmail.com>
2019-12-18 15:47:47 -0800

    implemented custom scopes for portfolios

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,3 @@
 class Skill < ApplicationRecord
-	validates_presence_of :title, :validates_presence_of
+	validates_presence_of :title, :percent_utilized
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+3.times do |i|
+  Topic.create!(
+    title: "Topic #{i}"
+  )
+end
+
 10.times do |i|
   Blog.create!(
   	title: "My Blog Post #{i}",
-  	body:  "Build out the portfolio feature to allow for an image, a title, a link, and a description."
+  	body:  "Build out the portfolio feature to allow for an image, a title, a link, and a description.",
+    topic: Topic.last
   )
 end
 puts "10 blog posts created"
@@ -22,13 +29,24 @@ puts "10 blog posts created"
 end
 puts "5 skills created"
 
-9.times do |i|
+8.times do |i|
   Portfolio.create!(
-    title: "Portfolio title #{i}",
-    subtitle: "My greate service",
+    title: "Ruby on Rails Portfolio title #{i}",
+    subtitle: "Ruby on Rails",
     body: "Build out the portfolio feature to allow for an image, a title, a link, and a description.",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200"
   )
 end
-puts "9 portfolios created"
+puts "8 Ruby on Rails portfolios created"
+
+1.times do |i|
+  Portfolio.create!(
+    title: "Angular Portfolio title #{i}",
+    subtitle: "Angular",
+    body: "Build out the portfolio feature to allow for an image, a title, a link, and a description.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+puts "1 Angular portfolios created"
