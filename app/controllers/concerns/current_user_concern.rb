463702dee5411bbc441615bf30c06e468051510f
cwang <cwang@hotmail.com>
2020-03-27 16:21:41 -0700

    Added guest user for current user

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,15 @@
+module CurrentUserConcern
+  extend ActiveSupport::Concern
+
+  def current_user
+    super || guest_user
+  end
+
+  private
+
+  def guest_user
+    OpenStruct.new(first_name: 'Guest',
+                   last_name: 'User'
+                  )
+  end
+end
